3f6437dd668cec291ff5a250d9256700236ca5c8
Angel <castorenalugo@gmail.com>
2018-11-04 17:56:49 -0600

    Rest server-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-//=============
-// Puerto
-//=============
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Jerga123@ds151863.mlab.com:51863/cafe';
+}
+process.env.URLDB = urlDB;
