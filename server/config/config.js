bc68f9ae64669ab7a20be2e404814d5815aa8e33
Angel <castorenalugo@gmail.com>
2018-11-04 19:00:44 -0600

    Variable de entorno MONGO3

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //urlDB = process.env.NODE_ENV.MONGO_URI;
-    urlDB = 'mongodb://cafe-user:Jerga123@ds151863.mlab.com:51863/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
